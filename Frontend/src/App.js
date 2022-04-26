0b32960a483f6cc7c68d26d2ef0f9fbdad8e1e98
crisperezv <crperez2018@udec.cl>
2022-04-25 23:41:29 -0400

    no hice na

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+import ListGroup from 'react-bootstrap/ListGroup'
 
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Bienvenido, Cristian</h1>
+        <div>
+          <Button variant="primary" size="lg">Agregar</Button>{' '}
+          <Button variant="secondary" size="lg">Cerrar sesión</Button>{' '}
+        </div>
+        <div>
+          <ListGroup>
+            <ListGroup.Item action> Encuesta 1 
+              <Button>
+                Editar
+              </Button>
+            </ListGroup.Item>
+            <ListGroup.Item action> Encuesta 2 </ListGroup.Item>
+            <ListGroup.Item action> Encuesta 3 </ListGroup.Item>
+          </ListGroup>
+        </div>
+      </header>
+      
+    </div>
+  );
+}
+
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hoy es un gran día.
         </p>
         <a
           className="App-link"
@@ -21,5 +51,5 @@ function App() {
     </div>
   );
 }
-
+*/ 
 export default App;
