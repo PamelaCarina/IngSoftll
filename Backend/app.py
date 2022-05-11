118de70e3ddbc76859894be010472a8fee6ed6fd
SurturTawers <davtorresga@gmail.com>
2022-05-11 11:24:29 -0400

    DB se llama is2flask ahora, insertar en BD un editor con id 1 para que funcione, guarda la encuesta con una fecha de creación fija (falta timestamp)

--- Backend/app.py
+++ Backend/app.py
@@ -1,25 +1,20 @@
-import json
-
 from flask import Flask, jsonify, request
-from flask import render_template
-from flask_sqlalchemy import SQLAlchemy #lo mismo para sqlalchemy
-from sqlalchemy import func
+from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from pip import main
 
+####### APP CONFIG (APP, DB, MAIL) #######
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flaskmysql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/is2flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) #le pasaremos los datos de arriba al ORM, una vez esto se ejecute nos entregara una ainstancia de una base de datos guardada en la vairable db
 ma = Marshmallow(app) #instancia del modulo de marshmallow que nos permiote la interaccion
 
-""" 
+####### MODELOS Y SCHEMAS #######
 Tag_encuesta = db.Table('Tag_encuesta', #TABLA MANY TO MANY QUE RELACIONA A ENCUESTADO CON ENCUESTA
     db.Column('id_tag', db.Integer, db.ForeignKey('tag.id_tag')),
     db.Column('id_encuesta', db.Integer, db.ForeignKey('encuesta.id_encuesta'))
 )
-
 
 Contesta_encuesta = db.Table('Contesta_encuesta',  #TABLA MANY TO MANY QUE RELACIONA A ENCUESTADO CON ENCUESTA
     db.Column('id_encuestado', db.Integer, db.ForeignKey('encuestado.id_encuestado')),
@@ -27,95 +22,115 @@ Contesta_encuesta = db.Table('Contesta_encuesta',  #TABLA MANY TO MANY QUE RELAC
     db.Column('fecha_contestacion', db.String(30))
 )
 
-# TABLAS DE LA BASE DE DATOS
 class Encuestado(db.Model):  # CLASE ENCUESTADO
     id_encuestado = db.Column(db.Integer, primary_key=True)
-    correo = db.Column(db.String(40))
+    correo_encuestado = db.Column(db.String(40))
     encuestas = db.relationship('Encuesta', secondary=Contesta_encuesta, backref=db.backref('Encuestados_backref'),
                                 lazy='dynamic')
 
-    def __init__(self, correo, ):
-        self.correo = correo
+    def __init__(self, id_encuestado, correo_encuestado):
+        self.id_encuestado = id_encuestado
+        self.correo_encuestado = correo_encuestado
 
 
 class Tag(db.Model): #CLASE TAG
-    id_tag = db.Column(db.Integer, primary_key = True)
-    tag = db.Column(db.String(20))
-    encuestas = db.relationship('Encuesta', secondary = Tag_encuesta, backref = db.backref('Tags_backref'), lazy = 'dynamic')
+    id_tag = db.Column(db.Integer, primary_key=True)
+    nombre_tag = db.Column(db.String(20), unique=True)
+    encuestas = db.relationship('Encuesta', secondary=Tag_encuesta, backref=db.backref('Tags_backref'), lazy = 'dynamic')
 
-    def __init__(self, tag,):
-        self.tag = tag
+    def __init__(self, id_tag, nombre_tag):
+        self.id_tag = id_tag
+        self.nombre_tag = nombre_tag
+
+
 class Editor(db.Model): #CLASE EDITOR
-    id_editor = user = db.Column(db.Integer, primary_key = True)
-    user = db.Column(db.String(30))
-    password = db.Column(db.String(30), unique = True)
-    encuestas = db.relationship('Encuesta', backref = 'editor')
+    id_editor = db.Column(db.Integer, primary_key=True)
+    correo_editor = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(30), unique=True)
+    encuestas = db.relationship('Encuesta', backref='editor')
 
-    def __init__(self, user, password):
-        self.user = user
+    def __init__(self, id_editor, correo_editor, password):
+        self.id_editor = id_editor
+        self.correo_editor = correo_editor
         self.password = password
-"""
-
-class Encuesta(db.Model): #CLASE ENCUESTA
-    id_encuesta = db.Column(db.Integer, primary_key = True)
-    titulo_encuesta = db.Column(db.String(200))
-    descripcion_encuesta = db.Column(db.String(200))
-    #fecha_creacion = db.Column(db.String(30))
-    #user = db.Column(db.String(30), unique = True)
-    #encuestados =db.relationship('Encuestado', secondary = Contesta_encuesta, backref = db.backref('encuestas_backref'), lazy = 'dynamic')
-    #tags =db.relationship('Tag', secondary = Tag_encuesta, backref = db.backref('encuestas_backref'), lazy = 'dynamic')
-    #id_editor = db.Column(db.Integer, db.ForeignKey('editor.id_editor'))
-    preguntas = db.relationship('Pregunta', backref = 'encuesta')
-
-    def __init__(self, id_encuesta, titulo_encuesta, descripcion_encuesta):
-        self.id_encuesta = id_encuesta
-        self.titulo_encuesta = titulo_encuesta
-        self.descripcion_encuesta= descripcion_encuesta
-        #self.fecha_creacion = fecha_creacion
-        #self.user = user
 
 
 class Pregunta(db.Model):  # CLASE PREGUNTA
     id_pregunta = db.Column(db.Integer, primary_key=True)
-    enunciado = db.Column(db.String(200))
     id_encuesta = db.Column(db.Integer, db.ForeignKey('encuesta.id_encuesta'))
+    enunciado_pregunta = db.Column(db.String(200))
     alternativas = db.relationship('Alternativa', backref='pregunta')
 
-    def __init__(self, id_encuesta,id_pregunta, enunciado):
-        self.id_encuesta = id_encuesta
+    def __init__(self, id_pregunta, id_encuesta, enunciado_pregunta):
         self.id_pregunta = id_pregunta
-        self.enunciado = enunciado
+        self.id_encuesta = id_encuesta
+        self.enunciado_pregunta = enunciado_pregunta
 
 
 class Alternativa(db.Model): #CLASE ALTERNATIVA
-    id_alternativa = db.Column(db.Integer, primary_key = True)
-    enunciado = db.Column(db.String(200))
-    contador = db.Column(db.String(30))
+    id_alternativa = db.Column(db.Integer, primary_key=True)
     id_pregunta = db.Column(db.Integer, db.ForeignKey('pregunta.id_pregunta'))
+    enunciado_alternativa = db.Column(db.String(200))
+    contador = db.Column(db.String(30))
 
-    def __init__(self, id_alternativa, id_pregunta, enunciado, contador):
+    def __init__(self, id_alternativa, id_pregunta, enunciado_alternativa, contador):
         self.id_alternativa = id_alternativa
         self.id_pregunta = id_pregunta
-        self.enunciado = enunciado
+        self.enunciado_alternativa = enunciado_alternativa
         self.contador = contador
+
+
+class Encuesta(db.Model): #CLASE ENCUESTA
+    id_encuesta = db.Column(db.Integer, primary_key=True)
+    id_editor = db.Column(db.Integer, db.ForeignKey('editor.id_editor'))
+    titulo_encuesta = db.Column(db.String(200))
+    descripcion_encuesta = db.Column(db.String(200))
+    fecha_creacion = db.Column(db.String(30))
+    encuestados = db.relationship('Encuestado', secondary=Contesta_encuesta, backref=db.backref('encuestas_backref'), lazy = 'dynamic')
+    tags = db.relationship('Tag', secondary=Tag_encuesta, backref=db.backref('encuestas_backref'), lazy='dynamic')
+    preguntas = db.relationship('Pregunta', backref='encuesta')
+
+    def __init__(self, id_encuesta, id_editor, titulo_encuesta, descripcion_encuesta, fecha_creacion):
+        self.id_encuesta = id_encuesta
+        self.id_editor = id_editor
+        self.titulo_encuesta = titulo_encuesta
+        self.descripcion_encuesta= descripcion_encuesta
+        self.fecha_creacion = fecha_creacion
 
 
 db.create_all() #con este metodo crearemos todas nuestras tablas al instante
 
 
+class EditorSchema(ma.Schema):
+    class Meta: #definimos una clase meta
+        fields = ('id_editor', 'correo_editor', 'password') #campos que queremos obtener cada vez que interactuemos con este esquema
+
+editor_schema = EditorSchema() #instancia de un task schema
+editor_schema = EditorSchema(many=True) #instancia en caso de querer crear mas de un task schema
+
+
+class TagSchema(ma.Schema):
+    class Meta: #definimos una clase meta
+        fields = ('id_tag', 'nombre_tag') #campos que queremos obtener cada vez que interactuemos con este esquema
+
+tag_schema = TagSchema() #instancia de un task schema
+tag_schema = TagSchema(many=True) #instancia en caso de querer crear mas de un task schema
+
+
 class EncuestadoSchema(ma.Schema):
     class Meta: #definimos una clase meta
-        fields = ('id_encuestado', 'correo') #campos que queremos obtener cada vez que interactuemos con este esquema
+        fields = ('id_encuestado', 'correo_encuestado') #campos que queremos obtener cada vez que interactuemos con este esquema
 
 encuestado_schema = EncuestadoSchema() #instancia de un task schema
-encuestados_schema = EncuestadoSchema(many = True) #instancia en caso de querer crear mas de un task schema
+encuestados_schema = EncuestadoSchema(many=True) #instancia en caso de querer crear mas de un task schema
 
 
 class EncuestaSchema(ma.Schema):
     class Meta: #definimos una clase meta
-        fields = ('id_encuesta', 'titulo_encuesta', 'descripcion_encuesta') #campos que queremos obtener cada vez que interactuemos con este esquema
+        fields = ('id_encuesta', 'id_editor', 'titulo_encuesta', 'descripcion_encuesta', 'fecha_creacion') #campos que queremos obtener cada vez que interactuemos con este esquema
 
 encuesta_schema = EncuestaSchema()
+encuesta_schema = EncuestaSchema(many=True)
 
 
 class PreguntaSchema(ma.Schema):
@@ -123,44 +138,83 @@ class PreguntaSchema(ma.Schema):
         fields = ('id_pregunta', 'id_encuesta', 'enunciado_pregunta') #campos que queremos obtener cada vez que interactuemos con este esquema
 
 pregunta_schema = PreguntaSchema()
+pregunta_schema = PreguntaSchema(many=True)
 
 
 class AlternativaSchema(ma.Schema):
     class Meta: #definimos una clase meta
-        fields = ('id_alternativa', 'id_pregunta', 'enunciado_alternativa', 'contador_alternativa') #campos que queremos obtener cada vez que interactuemos con este esquema
+        fields = ('id_alternativa', 'id_pregunta', 'enunciado_alternativa', 'contador') #campos que queremos obtener cada vez que interactuemos con este esquema
 
 alternativa_schema = AlternativaSchema()
+alternativa_schema = AlternativaSchema(many=True)
 
+####### RUTAS #######
+
+###ENCUESTADO###
+@app.route("/saveRespuestas")
+@app.route("/showEncuesta")
+
+###EDITOR###
+@app.route("/login")
+@app.route("/signIn")
+@app.route("/listadoEncuestas")
+@app.route("/listadoEncuestados")
+@app.route("/sendEMails")
+@app.route("/editEncuesta")
+@app.route("/deleteEncuesta")
+@app.route("/editEncuestado")
+@app.route("/deleteEncuestado")
+@app.route("/ingresarEncuestado")
 
 @app.route("/saveEncuesta", methods=['POST'])
 def saveEncuesta():
     if request.method == 'POST':
+        #Extraigo el JSON de la request
         data = request.get_json()
-        #id_encuesta = data['id_encuesta']
-        id_encuesta = db.session.query(Encuesta).select_from(Encuesta).count()
+        #Se obtienen los id máximos de las encuestas, preguntas y alternativas
+        id_encuesta = db.session.query(Encuesta).select_from(Encuesta).count()+1
         max_id_pregunta = db.session.query(Pregunta).select_from(Pregunta).count()+1
-        max_id_alternativa = db.session.query(Alternativa).select_from(Pregunta).count()+1
+        max_id_alternativa = db.session.query(Alternativa).select_from(Alternativa).count()+1
+        #Se extraen los daots de la encuesta
         titulo_encuesta = data['titulo_encuesta']
         descripcion_encuesta = data['descripcion_encuesta']
-        new_encuesta = Encuesta(id_encuesta+1, titulo_encuesta, descripcion_encuesta)
-        #for t in data['tag_encuesta']:
-        #    new_tag = Tag_encuesta(t['id_tag'], id_encuesta)
-        #    db.session.add(new_tag)
-        #tag_encuesta = data['tag_encuesta']
+        #Se crea una nueva encuesta
+        fecha_creacion = "11-05-2022"
+        new_encuesta = Encuesta(id_encuesta, 1, titulo_encuesta, descripcion_encuesta, fecha_creacion)
+        #Se extraen los tags de la encuesta
+        """
+        se necesita:
+            recibir todos los tags
+            verificar si el tag existe
+            almacenar los tags en:
+                TagEncuesta
+                Tag
+        for t in data['tag_encuesta']:
+            new_tag = Tag_encuesta(t['id_tag'], id_encuesta)
+            db.session.add(new_tag)
+        tag_encuesta = data['tag_encuesta']
+        """
+        #Se añade el objeto encuesta a la BD
         db.session.add(new_encuesta)
+        #Se itera por la preguntas
         for p in data['preguntas']:
+            #Se extrane los datos de las preguntas
             #id_pregunta = p['id_pregunta']
             enunciado_pregunta = p['enunciado_pregunta']
-            new_pregunta = Pregunta(max_id_pregunta, id_encuesta+1, enunciado_pregunta)
+            #Se crea la nueva pregunta y se almacena
+            new_pregunta = Pregunta(max_id_pregunta, id_encuesta, enunciado_pregunta)
             db.session.add(new_pregunta)
+            #Se itera por las alternativas de una pregunta
             for a in p['alternativas']:
+                #Se extraen los datos de las alternativas
                 #id_alternativa = a['id_alternativa']
                 enunciado_alternativa = a['enunciado_alternativa']
-                contador_alternativa = a['contador_alternativa']
-                new_alternativa = Alternativa(max_id_alternativa, max_id_pregunta, enunciado_alternativa, contador_alternativa)
+                #Se crea la nueva alternativa y se almacena
+                new_alternativa = Alternativa(max_id_alternativa, max_id_pregunta, enunciado_alternativa, 0)
                 db.session.add(new_alternativa)
                 max_id_alternativa += 1
             max_id_pregunta += 1
+        #Se guardan los cambios realizados en la BD
         db.session.commit()
         return "ok"
 
