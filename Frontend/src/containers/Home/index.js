65a51743f476aff67ce4dc948f32b60a83fc012b
Pamela Carina Pinilla Banchero <pamela.pinilla98@gmail.com>
2022-06-01 21:11:44 -0400

    login agregado, aun no conecta con la base de datos

--- Frontend/src/containers/Home/index.js
+++ Frontend/src/containers/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
-//import FormLogin from '../../components/FormLogin';
+import FormLogin from '../../components/FormLogin';
 import MyNavbar from "../../components/Navbar";
 import MyFooter from "../../components/Footer";
 
@@ -9,9 +9,8 @@ const Home = () =>{
         <>
             <MyNavbar/>
             <h1>Bienvenido a Surveycado</h1>
-            <Container style={{marginTop: "150px", marginBottom:"350px"}}>
-            <h1>hola soy un login</h1>
-            {/* <FormLogin/> */}
+            <Container style={{marginTop: "80px"}}>
+                <FormLogin/>
             </Container>
             {/* <MyFooter/> */}
         </>
